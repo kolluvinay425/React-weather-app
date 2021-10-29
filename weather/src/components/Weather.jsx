80e2fca51584f56b8b2793dbc0e9bf4dd7c44ab3
kolluvinay425 <kolluvinay425@gmail.com>
2021-10-29 16:59:19 +0200

    update

--- weather/src/components/Weather.jsx
+++ weather/src/components/Weather.jsx
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { icons } from "react-icons/lib";
 
 export default function Weather({ d }) {
-const [icons setIcons] = useState([{
-    Thunderstorm:"wi-thunderstorm",
-    Drizzle:"wi-sleet",
-    Rain:wi-"snow",
-    Atmosphere:"wi-fog",
-    Clear:"wi-day-sunny",
-    Clouds:"wi-day-fog",
-}])
+  // const [Thunderstorm,set]
+  // const [icons setIcons] = useState([{
+  //     Thunderstorm:"wi-thunderstorm",
+  //     Drizzle:"wi-sleet",
+  //     Rain:wi-"snow",
+  //     Atmosphere:"wi-fog",
+  //     Clear:"wi-day-sunny",
+  //     Clouds:"wi-day-fog",
+  // }])
 
   const temperature = (temp) => {
     const inDegrees = Math.floor(temp - 273.15);
@@ -27,10 +28,13 @@ const [icons setIcons] = useState([{
   };
   return (
     <>
+      <h3>Weather condition in {d.city.name}</h3>
       <div className="col-2">
         {temperature(d.main.temp)}
-        <i className={`wi ${icon(d.weather[0].id)} display-1`}></i>
-        {/* {icon(d.weather[0].id)} */}
+        <i className="wi wi-day-sunny display-1"></i>
+        {/* <i className={`wi ${icon(d.weather[0].id)} display-1`}></i>
+        {icon(d.weather[0].id)} */}
+        <p>{d.dt_txt}</p>
       </div>
     </>
   );
